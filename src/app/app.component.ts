6126ebfa11eb5aed0c838cca1186f04324480b5d
jani <jfreire84@hotmail.com>
2020-03-18 17:29:00 +0100

    Contruccion de la página principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cloudclinicas';
+  title: string = 'cloudclinicas';
+  curso: string = "Informatica";
+  profesor: string = "Marco Aurelio"
 }
