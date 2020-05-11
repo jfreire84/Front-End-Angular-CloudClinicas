ee1bcbab6ed72532999543417268ad92b31bac6c
jani <jfreire84@hotmail.com>
2020-05-11 15:57:27 +0200

    Home nueva

--- src/app/paciente/ficha/ficha.component.ts
+++ src/app/paciente/ficha/ficha.component.ts
@@ -31,45 +31,7 @@ export class FichaComponent implements OnInit {
     })
   }
 
-  //Método para borrar la factura
-  delete(factura: Facturas): void{
-
-    //Botón si está seguro que desea eliminar la factura de Sweetalert2.
-    const swalWithBootstrapButtons = Swal.mixin({
-      customClass: {
-        confirmButton: 'btn btn-success',
-        cancelButton: 'btn btn-danger'
-      },
-      buttonsStyling: false
-    })
-    
-    swalWithBootstrapButtons.fire({
-      title: '¿Estás seguro?',
-      text:  `¿Seguro que deseas borrar el paciente ${factura.descripcion}`,
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Si, eliminar!',
-      cancelButtonText: 'No, cancelar!',
-      reverseButtons: true
-    }).then((result) => {
-      if (result.value) {
-        this.facturaService.borrarFactura(factura.id).subscribe(
-          response => {
-            this.facturas = this.facturas.filter(fac => fac !== factura)
-            swalWithBootstrapButtons.fire(
-              'Borrado!',
-              `El ${factura.descripcion} ha sido borrado con éxito`,
-              'success'
-            )
-          }
-        )
-        
-      } 
-      
-    })
-  }
-
-
+  
   
 
   }
